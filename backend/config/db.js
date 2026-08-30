cb587b7db357f7fc8d6ad3101388c68b9518fe31
Lakshan Perera <sachinthaperera937@gmail.com>
2026-08-30 11:22:39 +0530

    Fix auth cache and Mongo database target

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,9 +7,9 @@ const connectDB = async () => {
     }
 
     try {
-        const conn = await mongoose.connect(
-            process.env.MONGO_URI
-        );
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            dbName: process.env.MONGO_DB_NAME || "smartfarm"
+        });
 
         console.log(
             `MongoDB connected: ${conn.connection.host}`
